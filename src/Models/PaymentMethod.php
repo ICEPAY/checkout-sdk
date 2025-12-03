35dde9c6d495a967e3298dede799da62ae7c143d
thomashondema <thomas.hondema@icepay.com>
2025-12-03 08:57:22 +0100

    Update PaymentMethod.php

--- src/Models/PaymentMethod.php
+++ src/Models/PaymentMethod.php
@@ -11,16 +11,32 @@ class PaymentMethod
     const TYPE_PAYPAL = 'paypal';
     const TYPE_EPS = 'EPS';
     const TYPE_PAYBYBANK = 'paybybank';
-    public function __construct(public string $type){
+    public function __construct(public string $type, public ?string $issuer = null)
+    {
+    }
+    public function withType(string $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+    public function withIssuer(string $issuer): self
+    {
+        $this->issuer = $issuer;
+        return $this;
     }
     public static function fromArray(array $data): self
     {
-        return new self($data['type']);
+        return new self($data['type'], $data['issuer'] ?? null);
     }
     public function toArray(): array
     {
-        return [
+        $result = [
             'type' => $this->type,
         ];
+
+        if ($this->issuer !== null) {
+            $result['issuer'] = $this->issuer;
+        }
+        return $result;
     }
 }
