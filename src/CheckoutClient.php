ea0a318977db3e1d63dd05016953db019f1329b4
thomashondema <thomas.hondema@icepay.com>
2025-12-04 09:03:50 +0100

    Update CheckoutClient.php

--- src/CheckoutClient.php
+++ src/CheckoutClient.php
@@ -2,6 +2,7 @@
 
 namespace ICEPAY\Checkout;
 
+use ICEPAY\Checkout\Models\JsonDeserializable;
 use ICEPAY\Checkout\Models\Request\Checkout as CheckoutRequest;
 use ICEPAY\Checkout\Models\Response\Checkout as CheckoutResponse;
 use ICEPAY\Checkout\Models\Request\Refund as RefundRequest;
@@ -29,22 +30,25 @@ class CheckoutClient
     // POST: https://checkout.icepay.com/api/payments
     public function checkout(CheckoutRequest $checkout): CheckoutResponse
     {
-        $response = $this->httpClient->post(self::BASE_URL . 'api/payments', $checkout);
-        $this->checkStatusCode($response);
-        $json = $response->getBody()->__toString();
-        $checkoutResponse = CheckoutResponse::fromResponse($json);
-        return $checkoutResponse;
+        /** @var CheckoutResponse $result */
+        $result = $this->callCheckoutApi(self::BASE_URL . 'api/payments', CheckoutResponse::class, $checkout);
+        return $result;
+
+
+//        $response = $this->httpClient->post(self::BASE_URL . 'api/payments', $checkout);
+//        $this->checkStatusCode($response);
+//        $json = $response->getBody()->__toString();
+//        $checkoutResponse = CheckoutResponse::fromResponse($json);
+//        return $checkoutResponse;
     }
 
     // POST https://checkout.icepay.com/api/payments/{id}/refund
-    public function refund(RefundRequest $refund, string $checkoutId)
+    public function refund(RefundRequest $refund, string $checkoutId) : RefundResponse
     {
-        $response = $this->httpClient->post(self::BASE_URL . 'api/payments/' . $checkoutId . '/refund', $refund->toArray());
-        if($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) {
-            throw new \Exception("Refund creation failed with status code: " . $response->getStatusCode());
-        }
+        $response = $this->httpClient->post(self::BASE_URL . 'api/payments/' . $checkoutId . '/refund', $refund);
+        $this->checkStatusCode($response);
         $json = $response->getBody()->__toString();
-        $refundResponse = RefundResponse::fromResponse(json_decode($json, true));
+        $refundResponse = RefundResponse::fromResponse($json);
         return $refundResponse;
     }
 
@@ -52,11 +56,9 @@ class CheckoutClient
     public function getCheckout(string $checkoutId): CheckoutResponse
     {
         $response = $this->httpClient->get(self::BASE_URL . 'api/payments/' . $checkoutId);
-        if($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) {
-            throw new \Exception("Get checkout failed with status code: " . $response->getStatusCode());
-        }
+        $this->checkStatusCode($response);
         $json = $response->getBody()->__toString();
-        $checkoutResponse = CheckoutResponse::fromResponse(json_decode($json, true));
+        $checkoutResponse = CheckoutResponse::fromResponse($json);
         return $checkoutResponse;
     }
 
@@ -71,7 +73,21 @@ class CheckoutClient
         $methods = json_decode($json, true);
         return $methods;
     }
+    protected function callCheckoutApi($url, string $className, ?\JsonSerializable $payload = null): JsonDeserializable
+    {
+        if (is_subclass_of($className, JsonDeserializable::class)){
+            throw new \Exception("Class $className is not a subclass of JsonDeserializable");
+        }
+        if($payload !== null) {
+            $response = $this->httpClient->post($url, $payload);
+        } else {
+            $response = $this->httpClient->get($url);
+        }
+        $this->checkStatusCode($response);
+        $json = $response->getBody()->__toString();
 
+        return $className::fromResponse($json);
+    }
     protected function checkStatusCode(ResponseInterface $response): bool
     {
         $statusCode = $response->getStatusCode();
