a5ae06b122a0a6bb5df8b593d4c6c5beb698d9f7
thomashondema <thomas.hondema@icepay.com>
2025-12-04 08:49:05 +0100

    remove duplicate code

--- src/Models/Response/Forward.php
+++ src/Models/Response/Forward.php
@@ -11,26 +11,4 @@ use ICEPAY\Checkout\Models\Status;
 class Forward extends \ICEPAY\Checkout\Models\Forward
 {
     public Payment $payment;
-
-    public static function fromResponse(array|string $data): self
-    {
-        if(is_string($data)) {
-            $data = json_decode($data, true);
-        }
-
-        return self::fromArray($data);
-    }
-
-    public static function fromArray(array $data): self{
-        $result = new self();
-
-        $result->key = $data['key'];
-        $result->status = Status::fromString($data['status']);
-        $result->amount = new Amount($data['amount']['value'], $data['amount']['currency'] ?? null);
-        $result->reference = $data['reference'];
-        $result->description = $data['description'];
-        $result->recipient = Recipient::fromArray($data['recipient']);
-
-        return $result;
-    }
 }
--- src/Models/Response/Refund.php
+++ src/Models/Response/Refund.php
@@ -8,24 +8,4 @@ use ICEPAY\Checkout\Models\Payment;
 class Refund extends \ICEPAY\Checkout\Models\Refund
 {
     public Payment $payment;
-
-    public static function fromResponse(array|string $data): static
-    {
-        if(is_string($data)) {
-            $data = json_decode($data, true);
-        }
-
-        return parent::fromArray($data);
-    }
-
-    public static function fromArray(array $data): static{
-        $result = parent::fromArray($data);
-
-        return $result;
-    }
-
-    public function toArray(): array
-    {
-        // TODO: Implement toArray() method.
-    }
 }
