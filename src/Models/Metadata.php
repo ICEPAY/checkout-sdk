daa0f8e1e1358bebc733fc84c056084729d546b6
Thomas Hondema <95758114+Thomas-ICEPAY@users.noreply.github.com>
2025-12-09 16:39:39 +0100

    add utility functions to add integration information (#6)

--- src/Models/Metadata.php
+++ src/Models/Metadata.php
@@ -7,13 +7,11 @@ class Metadata extends JsonDeserializable implements \JsonSerializable
     public function __construct(protected array $data = [])
     {
     }
-
     public function withCustomer(array $customer): self
     {
         $this->data['customer'] = $customer;
         return $this;
     }
-
     public function withCustomerEmail(string $email): self
     {
         if (!isset($this->data['customer'])) {
@@ -21,6 +19,15 @@ class Metadata extends JsonDeserializable implements \JsonSerializable
         }
 
         $this->data['customer']['email'] = $email;
+        return $this;
+    }
+    public function withIntegrationInformation(string $type, string $version, string $developer): self
+    {
+        $this->data['integration'] = array_merge($this->data['integration'] ?? [], [
+            'type' => $type,
+            'version' => $version,
+            'developer' => $developer,
+        ]);
         return $this;
     }
     public static function fromArray(array $data): static
